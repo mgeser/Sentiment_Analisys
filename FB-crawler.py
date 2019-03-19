967556da31a2f43801a1c8d58062f4599b7cf83b
geser <geser@Mr.Geser>
2019-03-19 12:24:22 +0700

    First Commit

---
+++ FB-crawler.py
@@ -0,0 +1,54 @@
+import facebook as fb
+from textblob import TextBlob
+import requests
+import numpy as np
+import pandas as pd
+import re
+
+corpus = []
+datasets = []
+
+graph = fb.GraphAPI(access_token="EAAgnVUGlUy4BAL1jcEVu8Yqz978h67EiK0cQUKPvcvZAhgtjVihzMZAKQgRCZClnhRDC2Cc5dhfOwLZAKyG6miLlBPI8V5KlmqKXgPexZAK7qh929uNfjpCzTRgS2kuvumreZAs8orgPdINIdybFxUJYrU4Lf2xSdv5NODpVjGk69SeNtmiH2ZCBGqt60W6wwwZD")
+
+# KATA KUNCI SEARCH
+key = '2019GantiPresiden'
+# LIMIT PENCARIAN (page, post, comment)
+limit = 5
+# REQUEST
+req = "/search?q=%s&type=page&limit=%s"%(key, limit)
+
+pages = graph.request(req)
+pagelist = pages['data']
+
+# dapatkan 5 post dari masing-masing page
+for page in pagelist:
+    posts = graph.request('/%s/posts?%s'%(page['id'], limit))
+    postslist = posts['data']
+    # dapatkan 5 comment dari masing-masing post
+    for post in postslist:
+        comments = graph.request('/%s/comments?%s'%(post['id'], limit))
+        commentlist = comments['data']
+        # dapatkan detail(text) dari masing-masing comment
+        for comment in commentlist:
+            # print comment['message']
+            comment_clean = re.sub('[^a-zA-Z]', ' ', comment['message'])
+            comment_clean = comment_clean.lower()
+            corpus.append(comment_clean)
+           
+            #message = TextBlob(comment['message'])
+            #datasets.append([comment['message'], message.sentiment.polarity])
+
+# print hasil
+
+for data in corpus:
+    if(TextBlob(data).sentiment.polarity <= 0):
+        sentiment = 'negative'
+    else: sentiment = 'positive'
+
+    datasets.append([data, sentiment])
+
+array = np.array(datasets)
+#np.savetxt('hasil.csv', array, delimiter=',')
+print array
+df = pd.DataFrame(array)
+df.to_csv('hasil.csv')
